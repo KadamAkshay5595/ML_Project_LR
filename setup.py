4fbc01b6771edb0e249f8a7d2c137b5e487f915d
akshay.kadam <akshay.kadam@pravaig.com>
2023-11-25 16:56:30 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT = '-e .'
+"""HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
@@ -12,15 +12,13 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements
+    return requirements"""
 
-setup (
-    
-    name="GemstonePricePrediction",
+setup(
+    name='GemstonePricePrediction',
     version='0.0.1',
-    author="Akshay Kadam",
-    author_email="akshayanandraokadam@gmail.com",
-    install_requires=get_requirements('requirements.txt'),
+    author='Akshay Kadam',
+    author_email='akshayanandraokadam@gmail.com',
+    install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
-    
 )
