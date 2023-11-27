c1b41571a1edcea8dfc60dfa3dc2bc3cccdb67c8
akshay.kadam <akshay.kadam@pravaig.com>
2023-11-27 14:00:35 +0530

    app development code

--- app.py
+++ app.py
@@ -6,3 +6,37 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return render_template("index.html")
+
+@app.route('/predict',methods=["GET","POST"])
+def predict_datapoint():
+    
+    if request.method=='GET':
+        return render_template('form.html')
+    
+    else:
+        data=CustomData(
+            
+            carat=float(request.form.get('carat')),
+            depth = float(request.form.get('depth')),
+            table = float(request.form.get('table')),
+            x = float(request.form.get('x')),
+            y = float(request.form.get('y')),
+            z = float(request.form.get('z')),
+            cut = request.form.get('cut'),
+            color= request.form.get('color'),
+            clarity = request.form.get('clarity')
+        )
+        # this is my final data
+        final_data=data.get_data_as_dataframe()
+        
+        predict_pipeline=PredictPipeline()
+        
+        pred=predict_pipeline.predict(final_data)
+        
+        result=round(pred[0],2)
+        
+        return render_template("result.html",final_result=result)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
