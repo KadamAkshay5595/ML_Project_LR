d512f9618c97aadbaafa972cdca56b7d87da09d2
akshay.kadam <akshay.kadam@pravaig.com>
2023-11-30 17:48:02 +0530

    model-evaluation added

--- src/GemstonePricePrediction/pipelines/training_pipeline.py
+++ src/GemstonePricePrediction/pipelines/training_pipeline.py
@@ -3,8 +3,7 @@ from src.GemstonePricePrediction.components.data_ingestion import DataIngestion
 from src.GemstonePricePrediction.components.data_transformation import DataTransformation
 
 from src.GemstonePricePrediction.components.model_trainer import ModelTrainer
-
-
+from src.GemstonePricePrediction.components.model_evaluation import ModelEvaluation
 
 import os
 import sys
@@ -23,3 +22,6 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
+
+model_eval_obj=ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
